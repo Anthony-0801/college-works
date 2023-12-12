36936096d70a12d22cdea28c5a76869459c06604
Anthony <zyxanthony15@gmail.com>
2023-12-12 22:58:54 +0800

    Adding another C file

---
+++ firstYear/C/The input is the output (b).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h.>
+int main()
+// Hi I'm Anthony, so I'm gonna make a calculating program  that simply solve four basic operations//
+{   printf ("Please type any words:");
+   char a[100];
+   fgets (a,100,stdin);
+   printf ("\nThe words you type are: %s", a);
+
+   return 0;
+}
