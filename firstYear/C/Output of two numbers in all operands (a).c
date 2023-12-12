34ec4670d2a72190d52e73dc088d72c2f33d265c
Anthony <zyxanthony15@gmail.com>
2023-12-12 22:55:33 +0800

    Adding directories and adding first C file

---
+++ firstYear/C/Output of two numbers in all operands (a).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h.>
+int main()
+// Hi I'm Anthony, so I'm gonna make a calculating program  that simply solve four basic operations//
+{
+    int a,b;
+   printf ("Please put two numbers:");
+   scanf ("%d %d", &a,&b);
+   printf ("Sum:\n %d",a+b);
+   printf ("\nDifference:\n %d",a-b);
+   printf ("\nProduct:\n %d",a*b);
+   printf ("\nDivision:\n %d",a/b);
+   return 0;
+}
