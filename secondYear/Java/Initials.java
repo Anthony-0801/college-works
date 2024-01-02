3335b9880ab5fe5ade08684a8bec99aff5e9ecb4
Anthony <zyxanthony15@gmail.com>
2024-01-02 23:21:25 +0800

    adding Java file

---
+++ secondYear/Java/Initials.java
@@ -0,0 +1,9 @@
+//Anthony A. Cabulang BSIT-2A
+public class Initials {
+    public static void main (String args []) {
+	char a1 = 'A';
+	char a2 = 'A';
+	char a3 = 'C';
+	System.out.println (a1 + "." + a2+ "." + a3+ ".");
+    }
+}
