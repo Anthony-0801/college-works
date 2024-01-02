75beacdabb22926108c175cb3b1866929f4e288e
Anthony <zyxanthony15@gmail.com>
2024-01-02 23:23:51 +0800

    adding Java file

---
+++ secondYear/Java/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+//Anthony A. Cabulang BSIT-2A
+public class MovieQuoteInfo {
+    public static void main (String args []) {
+	
+	System.out.println ("Favorite Movie Quote: You have to do the best with what God gave you.");
+	System.out.println ("From the Movie Of:    Forrest Gump.");
+	System.out.println ("The One Who Said It:  Mrs. Gump.");
+	System.out.println ("Year of the Movie:    1994.");
+    }
+}
