0af9290f0080a8b5e80fdd1c3725628dea881a9a
Anthony <zyxanthony15@gmail.com>
2024-03-07 21:37:34 +0800

    Create mean-of-array.php
    :+1: demn

---
+++ OJT/PHP/mean-of-array.php
@@ -0,0 +1,10 @@
+<?php
+//Anthony A. Cabulang
+$numbers = array (13, 143, 88, 65, 120);
+$mean = 0;
+for ($i = 0; $i < count($numbers); $i++) {
+    $mean += $numbers[$i];
+}
+$mean = $mean / count($numbers);
+echo  "The mean of the sequence is: " . $mean;
+?>
